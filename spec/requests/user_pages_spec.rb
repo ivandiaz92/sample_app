bd27195d02ea9eb709e63971bd881e359ce60f95
Ivan <ivandiazmtz@gmail.com>
2014-05-31 16:01:13 -0500

    Finish layout and routes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe "User pages" do
 
-	subject { page }
+  subject { page }
 
-	describe "signup page" do
-		before { visit signup_path }
+  describe "signup page" do
+    before { visit signup_path }
 
-		it { should have_content('Sign up') }
-		it { should have_title(full_title('Sign up')) }
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
 end
